058f17a36f450625b78908371a92088f622287b9
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2024-01-11 19:00:10 -0300

    Fix

--- src/Components/ui/boton.jsx
+++ src/Components/ui/boton.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Boton = ({ children, className = "", ...args }) => {
+const Boton = ({ children, className = "", ... args }) => {
     return (
         <div>
-            <button className={`rounded-xl py-2 px-3 bg-blue-400 text-center text-white ${className} {...args}`}>
+            <button className={`rounded-xl py-2 px-3 bg-blue-400 text-center text-white ${className}`} {...args}>
                 {children}
             </button>
         </div>
