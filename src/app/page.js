350f0167da90b1afebc863af3eb4ecf66e6180dc
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2024-01-12 15:40:53 -0300

    Metadata estatica y dinamica

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,34 @@ import Header from "@/Components/ui/header"
 // import Boton from "@/Components/ui/boton"
 import Image from "next/image" //para poder importar imagenes
 
+// optimizacion seo --> estatico
+export const metadata = { 
+  title: "CoderHouse - app (CP)",
+  description: "app de muestra en Next.js",
+  keywords: ["CoderHouse", "NextJS", "Vercel", "SSR"],
+  openGraph: {
+    title: "Next.js", 
+    description: "The React framework for the web",
+    type: "article", 
+    publishedTime: "2024-01-12",
+    authors: ["Cecilia Perdomo"]
+  }
+}
+
+// optimizacion seo --> dinamico
+// export async function generateMetadata ({params, searchParams}, parent) {
+//   const id = params.id
+//   const product = await fetch(`https//.../${id}`)
+//   .then(response => response.json())
+
+//   return (
+//     {
+//       title: product.title, 
+//       description: product.description
+//     }
+//   )
+// }
+
 export default function Home() {
   return (
     // m-auto: margen automatico
